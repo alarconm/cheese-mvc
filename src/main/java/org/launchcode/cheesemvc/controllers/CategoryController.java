c33c53431e5cd36564e93dde64d83eca5fb7c952
Michael Alarcon <malarconii@yahoo.com>
2017-09-12 20:02:25 -0700

    Completed Category model, added form to add a new category & configured controller.

--- src/main/java/org/launchcode/cheesemvc/controllers/CategoryController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CategoryController.java
@@ -1,11 +1,17 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Category;
 import org.launchcode.cheesemvc.models.data.CategoryDao;
 import org.launchcode.cheesemvc.models.data.CheeseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
@@ -23,6 +29,27 @@ public class CategoryController {
         model.addAttribute("categories", categorydao.findAll());
         model.addAttribute("title", "Categories");
         return "category/index";
+    }
 
+    @RequestMapping(value = "add")
+    public String addForm(Model model) {
+
+        Category category = new Category();
+        model.addAttribute("title", "Add Category");
+        model.addAttribute("category", category);
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            model.addAttribute("category", category);
+            return "category/add";
+        }
+
+        categorydao.save(category);
+        return "redirect:";
     }
 }
